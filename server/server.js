6c538a4060ac1eea522ab1fc56f44073db583280
Kartik <kartikagarwal20144@gmail.com>
2024-03-14 19:13:09 +0530

    Adding organization dashboard api code

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config();
-const dbConfig = require('./config/dbConfig');
-const usersRoutes = require('./routes/usersRoutes');
-const inventoryRoute = require('./routes/inventoryRoute')
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+
+const usersRoutes = require("./routes/usersRoutes");
+const inventoryRoute = require("./routes/inventoryRoute");
+const dashboardRoute = require("./routes/dashboardRoute");
 
 app.use(express.json());
 
-app.use('/api/users', usersRoutes)
-app.use('/api/inventory', inventoryRoute)
+app.use("/api/users", usersRoutes);
+app.use("/api/inventory", inventoryRoute);
+app.use("/api/dashboard", dashboardRoute);
 
-app.listen(port, () => console.log(`Node JS Serer started at ${port}`))
+app.listen(port, () => console.log(`Node JS Serer started at ${port}`));
